97a6a909cf9d23159feb315ae5b9b9d1206a9bad
Nick van Raaij <dagelijksnick@gmail.com>
2021-03-18 19:53:05 +0100

    Added Model for spel

---
+++ ReversiMvcApp/Models/Spel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReversiMvcApp.Models
+{
+    public class Spel
+    {
+        public string Token { get; set; }
+        public string Omschrijving { get; set; }
+        public string Speler1token { get; set; }
+        public string Speler2token { get; set; }
+    }
+}
